33d442efa7fb1de599b25191dbc945e583f0f2cf
bloom-lmh <1357526355@qq.com>
2024-10-17 11:28:52 +0800

    项目初始化搭建

---
+++ Person.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Person {
+    name;
+    constructor(name) {
+        this.name = name;
+    }
+    getName() {
+        this.name;
+    }
+    setName(name) {
+        this.name = name;
+    }
+    get age() {
+        return 22;
+    }
+    set age(age) {
+        this.age = age;
+    }
+}
+//# sourceMappingURL=Person.js.map
---
+++ demo.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Person {
+    name;
+    getName() {
+        return this.name;
+    }
+    setName(name) {
+        this.name = name;
+    }
+}
+//# sourceMappingURL=demo.js.map
