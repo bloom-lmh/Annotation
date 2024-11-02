50590cf2a196d0f54a74c141de166efcf9559771
bloom-lmh <1357526355@qq.com>
2024-11-02 14:57:08 +0800

    提交

--- src/annotation/annotation.ts
+++ src/annotation/annotation.ts
@@ -1,140 +1,112 @@
-import ts from "typescript";
-import { PickContext } from "../picker/pickcontext";
-import { ClassInfo, MethodInfo, PropertyInfo } from "../type";
-import * as vscode from 'vscode';
-import { ClassDeclaration, FunctionDeclaration, MethodDeclaration, Project, PropertyDeclaration, SourceFile } from "ts-morph";
+
+import { ClassDeclaration, FunctionDeclaration, MethodDeclaration, PropertyDeclaration } from "ts-morph";
 import { TsFileParser } from "../parser/tsFileParser";
 /**
  * 类注释类
  */
 export class ClassAnnotation {
-  /**
-* 首行位置
-*/
-  private startRow: number
-  /** 
-   * 类名
+    /**
+  * 首行位置
   */
-  private className: string
-  /**
-   * 是否抽象类
-   */
-  private isAbstract: boolean
+    private startRow: number
+    /** 
+     * 类名
+    */
+    private className: string
+    /**
+     * 是否抽象类
+     */
+    private isAbstract: boolean
 
-  /**
-   * 
-   * @param className 类名
-   * @param isAbstract 是否抽象类
-   */
-  constructor(memberDeclaration: ClassDeclaration) {
-    this.startRow = memberDeclaration.getStartLineNumber()
-    this.className = memberDeclaration.getName() || ''
-    this.isAbstract = memberDeclaration.isAbstract()
-  }
-  /**
-   * 创建注解
-   */
-  public createAnnotation(): string {
-    return `
-    ${this.className}
-   `
-
-  }
+    /**
+     * 
+     * @param className 类名
+     * @param isAbstract 是否抽象类
+     */
+    constructor(memberDeclaration: ClassDeclaration) {
+        this.startRow = memberDeclaration.getStartLineNumber()
+        this.className = memberDeclaration.getName() || ''
+        this.isAbstract = memberDeclaration.isAbstract()
+    }
+    /**
+     * 创建注解
+     */
+    public createAnnotation(): string {
+        return `\n${this.className}`
+    }
 }
 /**
  * 方法注释类
  */
 export class MethodAnnotation {
-  /**
- * 首行位置
- */
-  private startRow: number
-  /**
-   * 方法名
+    /**
+   * 首行位置
    */
-  private methodName: string
-  /**
-   * 方法参数
-   */
-  private parameters: Map<string, string>[]
-  /**
-   * 返回值
-   */
-  private returnType: string
-  /**
-   * 方法抛出的异常
-   */
-  private throwError: string | undefined
-  /**
-   * 
-   * @param memberDeclaration 方法声明
-   */
-  constructor(memberDeclaration: MethodDeclaration) {
-    this.startRow = memberDeclaration.getStartLineNumber()
-    this.methodName = memberDeclaration.getName()
-    this.parameters = TsFileParser.getMethodParameters(memberDeclaration)
-    this.returnType = memberDeclaration.getReturnType().getText()
-
-    // this.throwError = memberDeclaration.getThro
-  }
-  public createAnnotation(): string {
-    let paramStr = ''
-    for (const [paramName, paramType] of this.parameters) {
-      paramStr += `@param {${paramType}} - ${paramName}`
-      paramStr += '\n'
+    private startRow: number
+    /**
+     * 方法名
+     */
+    private methodName: string
+    /**
+     * 方法参数
+     */
+    private parameters: Map<string, string>
+    /**
+     * 返回值
+     */
+    private returnType: string
+    /**
+     * 方法抛出的异常
+     */
+    //private throwError: string
+    /**
+     * 
+     * @param memberDeclaration 方法声明
+     */
+    constructor(memberDeclaration: MethodDeclaration | FunctionDeclaration) {
+        this.startRow = memberDeclaration.getStartLineNumber()
+        this.methodName = memberDeclaration.getName() || ''
+        this.parameters = TsFileParser.getMethodParameters(memberDeclaration)
+        this.returnType = memberDeclaration.getReturnType().getText()
+        //this.throwError = 
+        TsFileParser.getMethodThrows(memberDeclaration)
     }
-    return `
-      ${this.methodName}
-      ${paramStr}
-      @returns {${this.returnType}}
-    `
-  }
+    public createAnnotation(): string {
+        let paramStr = ''
+        for (let [paramName, paramType] of this.parameters) {
+            paramStr += `@param {${paramType}} ${paramName}\n`
+        }
+        return `${this.methodName}\n${paramStr}@returns {${this.returnType}}`
+    }
 }
 
 /**
  * 属性注释类
  */
 export class PropertyAnnotation {
-  /**
-   * 首行位置
-   */
-  private startRow: number
+    /**
+     * 首行位置
+     */
+    private startRow: number
+    /** 
+     * 属性名
+    */
+    private propertyName: string
 
-  /**
-      * 编辑器对象
-      */
-  private editor: vscode.TextEditor
-  /** 
-   * 属性名
-  */
-  private propertyName: string
-
-  /**
-   * 属性类型
-   */
-  private propertyType: string
-
-  constructor(context: PickContext, member: PropertyInfo) {
-    this.startRow = member.startRow
-    this.propertyName = member.name
-    this.propertyType = member.propertyType || ''
-    this.editor = context.getEditor()
-  }
-  public createAnnotation(): void {
-    const document = this.editor.document
-    // 获取当前行的文本  
-    this.editor.edit(editBuilder => {
-      let positionAfterLine = new vscode.Position(this.startRow - 1, 0);
-      editBuilder.insert(positionAfterLine, `\n/**
- * 
- * ${this.propertyName}
- * @type {${this.propertyType}}
- */\n`);
-      vscode.workspace.save(document.uri)
-    }).then(success => {
-      if (!success) {
-        vscode.window.showErrorMessage('Failed to insert getter and setter methods.');
-      }
-    });
-  }
+    /**
+     * 属性类型
+     */
+    private propertyType: string
+    /**
+     * 
+     * @param memberDeclaration 方法声明
+     */
+    constructor(memberDeclaration: PropertyDeclaration) {
+        this.startRow = memberDeclaration.getStartLineNumber()
+        this.propertyName = memberDeclaration.getName()
+        this.propertyType = memberDeclaration.getType().getText()
+    }
+    public createAnnotation(): string {
+        return `${this.propertyName}\n@type {${this.propertyType}}`
+    }
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,44 +1,61 @@
 import * as vscode from 'vscode';
 import { Picker } from './picker/picker';
-import { ClassDeclaration, JSDoc, MethodDeclaration, Project, ts } from "ts-morph";
+import { ClassDeclaration, FunctionDeclaration, JSDoc, MethodDeclaration, Project, PropertyDeclaration, ts } from "ts-morph";
 import { TsFileParser } from './parser/tsFileParser';
-import { ClassAnnotation, MethodAnnotation } from './annotation/annotation';
+import { ClassAnnotation, MethodAnnotation, PropertyAnnotation } from './annotation/annotation';
 let m = new Map()
 export function activate(context: vscode.ExtensionContext) {
 
-  const disposable = vscode.commands.registerCommand('addAnnotation', () => {
-    // 获取文本编辑器
-    const editor = vscode.window.activeTextEditor;
-    // 获取失败提示信息
-    if (!editor) {
-      vscode.window.showErrorMessage("需选择类或方法代码");
-      return;
-    }
-    // 创建拾取器对象拾取光标所在单词和文件名以及行号
-    const picker = new Picker(editor)
-    const fileName = picker.pickFileName()
-    const wordText = picker.pickCursorWordText()
-    const lineNumber = picker.pickLineNumber()
-    // 解析ts文件为语法树
-    const sourceFile = TsFileParser.parse(fileName)
-    // 获取单词对应的成员信息
-    const memberDeclaration = TsFileParser.getMemberInfoByName(sourceFile, wordText, lineNumber);
-    // 生成注解
-    let jsdoc = ''
-    if (memberDeclaration instanceof ClassDeclaration) {
-      jsdoc = new ClassAnnotation(memberDeclaration).createAnnotation()
-    }
-    if (memberDeclaration instanceof MethodDeclaration) {
-      jsdoc = new MethodAnnotation(memberDeclaration).createAnnotation()
-    }
-    /*  if (memberDeclaration) {
-       let a = memberDeclaration.addJsDoc('')
-     } */
-    // 保存更改到文件  
-    sourceFile.saveSync();
-  });
+    const disposable = vscode.commands.registerCommand('addAnnotation', () => {
+        // 获取文本编辑器
+        const editor = vscode.window.activeTextEditor;
+        // 获取失败提示信息
+        if (!editor) {
+            vscode.window.showErrorMessage("需选择类或方法代码");
+            return;
+        }
+        // 创建拾取器对象拾取光标所在单词和文件名以及行号
+        const picker = new Picker(editor)
+        const fileName = picker.pickFileName()
+        const wordText = picker.pickCursorWordText()
+        const lineNumber = picker.pickLineNumber()
+        // 解析ts文件为语法树
+        const sourceFile = TsFileParser.parse(fileName)
+        // 获取单词对应的成员信息
+        const memberDeclaration = TsFileParser.getMemberInfoByName(sourceFile, wordText, lineNumber);
+        if (!memberDeclaration) return
+        // 生成注解
+        let jsdoc = ''
+        if (memberDeclaration instanceof ClassDeclaration) {
+            jsdoc = new ClassAnnotation(memberDeclaration).createAnnotation()
+        }
+        if (memberDeclaration instanceof MethodDeclaration || memberDeclaration instanceof FunctionDeclaration) {
+            jsdoc = new MethodAnnotation(memberDeclaration).createAnnotation()
 
-  context.subscriptions.push(disposable);
+        }
+        if (memberDeclaration instanceof PropertyDeclaration) {
+            jsdoc = new PropertyAnnotation(memberDeclaration).createAnnotation()
+        }
+        let a = memberDeclaration.addJsDoc(jsdoc)
+
+
+        // 添加 JSDoc 注释  
+        // 获取函数声明节点  
+
+        // 添加 JSDoc 注释  
+        /* const formatOptions: ts.FormatCodeSettings = {
+          indentSize: 2,
+          tabSize: 2,
+          newLineCharacter: "\n",
+          convertTabsToSpaces: true,
+        };
+        sourceFile.formatText(formatOptions) */
+        // 保存更改到文件  
+        sourceFile.saveSync();
+
+    });
+
+    context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
